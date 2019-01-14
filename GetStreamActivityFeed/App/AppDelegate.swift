115f1832bb6189859d83c3569a14f0789828c45b
Alexey Bukhtin <bukhtin@gmail.com>
2019-01-14 13:14:00 +0100

    Added the launch screen.

---
+++ GetStreamActivityFeed/App/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  GetStreamActivityFeed
+//
+//  Created by Alexey Bukhtin on 14/01/2019.
+//  Copyright © 2019 Stream.io Inc. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
